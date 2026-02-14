b55a5266a050f29617bb000ad7bf24fcfe5037b5
iamjmydv <127085144+iamjmydv@users.noreply.github.com>
2026-02-14 11:43:31 +0800

    Initial Project

---
+++ dart_practice_nn/bin/dart_practice_nn.dart
@@ -0,0 +1,5 @@
+import 'package:dart_practice_nn/dart_practice_nn.dart' as dart_practice_nn;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_practice_nn.calculate()}!');
+}
---
+++ dart_practice_nn/test/dart_practice_nn_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_practice_nn/dart_practice_nn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
