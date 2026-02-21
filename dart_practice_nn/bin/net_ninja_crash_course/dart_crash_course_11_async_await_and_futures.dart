211e7f06d9b8e6d651aa45e04b421b906ec5fd74
iamjmydv <127085144+iamjmydv@users.noreply.github.com>
2026-02-21 17:06:41 +0800

    Dart Crash Course #11 - Async, Await & Futures first sample

--- dart_practice_nn/bin/net_ninja_crash_course/dart_crash_course_11_async_await_and_futures.dart
+++ dart_practice_nn/bin/net_ninja_crash_course/dart_crash_course_11_async_await_and_futures.dart
@@ -1,6 +1,30 @@
 //------------------------------------------------------------------------------
 // F U T U R E - A S Y N C - A W A I T
-//------------------------------------------------------------------------------\
+//------------------------------------------------------------------------------
+
+// Futures --> like promises in JavaScript
+// it can have uncompleted or completed state.
+//------------------------------------------------------------------------------
+
 void main() {
-  
+  fetchPost().then((p){
+    print(p.title);
+    print(p.userId);
+  });
+}
+
+//RUNS 3 second before pass the value on .then() function.
+Future<Post> fetchPost() {
+  final delay = Duration(seconds: 3);
+  return Future.delayed(delay, (){
+    return Post("My Post", 123);
+  });
+}
+
+//Act's as Model/DTO
+class Post {
+  String title;
+  int userId;
+
+  Post(this.title, this.userId);
 }
