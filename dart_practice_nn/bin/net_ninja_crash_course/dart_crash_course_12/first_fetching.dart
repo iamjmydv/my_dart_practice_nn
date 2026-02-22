85bda737f1ffd1f69bcefd83c814d1f9b05f4f7c
iamjmydv <127085144+iamjmydv@users.noreply.github.com>
2026-02-23 01:25:29 +0800

    fixed identation of first_fetching.dart

--- dart_practice_nn/bin/net_ninja_crash_course/dart_crash_course_12/first_fetching.dart
+++ dart_practice_nn/bin/net_ninja_crash_course/dart_crash_course_12/first_fetching.dart
@@ -9,14 +9,14 @@ void main() async {
 
 Future<Post> fetchPost() async {
   var uri = Uri.https('jsonplaceholder.typicode.com', '/posts/1');
+
   final response = await http.get(uri, headers: {'Accept': 'application/json'});
 
   if (response.statusCode != 200) {
     throw Exception('Failed to fetch post: ${response.statusCode}');
   }
 
-  Map<String, dynamic> data =
-      convert.jsonDecode(response.body) as Map<String, dynamic>;
+  Map<String, dynamic> data = convert.jsonDecode(response.body) as Map<String, dynamic>;
 
   return Post(data["title"], data["userId"]);
 }
