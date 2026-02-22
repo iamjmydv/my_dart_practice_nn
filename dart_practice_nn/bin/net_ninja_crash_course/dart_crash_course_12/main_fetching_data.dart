4ec7c77bd37122bf9c3aac2efa1093e553a4184e
iamjmydv <127085144+iamjmydv@users.noreply.github.com>
2026-02-22 17:43:02 +0800

    Dart Crash Course #12 - Fetching Data Initial Setup

--- dart_practice_nn/bin/net_ninja_crash_course/dart_crash_course_12/main_fetching_data.dart
+++ dart_practice_nn/bin/net_ninja_crash_course/dart_crash_course_12/main_fetching_data.dart
@@ -1,3 +1,22 @@
-void main() {
-  
+//------------------------------------------------------------------------------
+// 
+//------------------------------------------------------------------------------
+void main() async {
+  final post = await fetchPost();
+  print(post.title);
+  print(post.userId);
+}
+
+Future<Post> fetchPost() {
+  final delay = Duration(seconds: 3);
+  return Future.delayed(delay, (){
+    return Post("My Post", 123);
+  });
+}
+
+class Post {
+  String title;
+  int userId;
+
+  Post(this.title, this.userId);
 }
