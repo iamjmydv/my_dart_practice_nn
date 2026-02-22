a1c42d0f49121349e614eeefdf2f8b979d25a91a
iamjmydv <127085144+iamjmydv@users.noreply.github.com>
2026-02-23 00:03:31 +0800

    Dart Crash Course #12 - Fetching Data f- added plain fetching data flow

---
+++ dart_practice_nn/bin/net_ninja_crash_course/dart_crash_course_12/first_fetching.dart
@@ -0,0 +1,29 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+void main() async {
+  final post = await fetchPost();
+  print('Title: ${post.title}');
+  print('UserId: ${post.userId}');
+}
+
+Future<Post> fetchPost() async {
+  var uri = Uri.https('jsonplaceholder.typicode.com', '/posts/1');
+  final response = await http.get(uri, headers: {'Accept': 'application/json'});
+
+  if (response.statusCode != 200) {
+    throw Exception('Failed to fetch post: ${response.statusCode}');
+  }
+
+  Map<String, dynamic> data =
+      convert.jsonDecode(response.body) as Map<String, dynamic>;
+
+  return Post(data["title"], data["userId"]);
+}
+
+class Post {
+  String title;
+  int userId;
+
+  Post(this.title, this.userId);
+}
