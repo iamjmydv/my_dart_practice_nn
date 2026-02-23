ad62e8f97e664108151cedf3e6fa58e6f4276226
iamjmydv <127085144+iamjmydv@users.noreply.github.com>
2026-02-23 13:45:27 +0800

    fetch data practice

--- dart_practice_nn/bin/general_dart_practice/http_get.dart
+++ dart_practice_nn/bin/general_dart_practice/http_get.dart
@@ -1,23 +1,23 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() {
+void main() async{
   fetchData();
 }
 
 Future<void> fetchData() async {
-  final uri = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
-
+  final uri = Uri.parse('https://jsonplaceholder.typicode.com/posts/2');
+ final response = await http.get(uri, headers: {'Accept': 'application/json'});
   try {
-    final response = await http.get(uri);
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = jsonDecode(response.body);
-      print('USER ID: ${data['userId']}');
-      print('TITLE: ${data['title']}');
-      print('BODY: ${data['body']}');
+   
+    if(response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      print(data['userId']);
+      print(data['title']);
+      print(data['body']);
     }
+
   } catch (e) {
-    print('Invalid get request $e');
+    print('Invalid get response with status code ${response.statusCode}');
   }
 }
-
