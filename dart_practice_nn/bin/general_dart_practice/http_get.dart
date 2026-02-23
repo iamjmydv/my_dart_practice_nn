13dd2237b238f40efc805490e3663481f30ba5c3
iamjmydv <127085144+iamjmydv@users.noreply.github.com>
2026-02-23 22:09:07 +0800

    update get with model

--- dart_practice_nn/bin/general_dart_practice/http_get.dart
+++ dart_practice_nn/bin/general_dart_practice/http_get.dart
@@ -1,23 +1,39 @@
+// https://jsonplaceholder.typicode.com/posts/2
+
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() async{
-  fetchData();
+void main() async {
+  final post = await fetchData();
+  print('User ID: ${post.userId}');
+  print('Title: ${post.title}');
+  print('Body: ${post.body}');
 }
 
-Future<void> fetchData() async {
-  final uri = Uri.parse('https://jsonplaceholder.typicode.com/posts/2');
- final response = await http.get(uri, headers: {'Accept': 'application/json'});
-  try {
-   
-    if(response.statusCode == 200) {
-      var data = jsonDecode(response.body);
-      print(data['userId']);
-      print(data['title']);
-      print(data['body']);
-    }
+Future<Post> fetchData() async {
+  final uri = Uri.parse("https://jsonplaceholder.typicode.com/posts/2");
 
+  try {
+    final response = await http.get(uri, headers: {'accept': 'application/json'});
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return Post(
+        userId: data["userId"],
+        title: data["title"],
+        body: data["body"],
+      );
+    } else {
+      throw Exception('Failed to fetch data: ${response.statusCode}');
+    }
   } catch (e) {
-    print('Invalid get response with status code ${response.statusCode}');
+    throw Exception('Invalid response: $e');
   }
+}
+
+class Post {
+  final int userId;
+  final String title;
+  final String body;
+
+  Post({required this.userId, required this.title, required this.body});
 }
