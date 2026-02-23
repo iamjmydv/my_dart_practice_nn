c506d4f81dd2c9770d4af7a2f5081a73b94e95fb
iamjmydv <127085144+iamjmydv@users.noreply.github.com>
2026-02-23 09:47:28 +0800

    misc files

--- dart_practice_nn/bin/net_ninja_crash_course/dart_crash_course_12/first_fetching.dart
+++ dart_practice_nn/bin/net_ninja_crash_course/dart_crash_course_12/first_fetching.dart
@@ -1,29 +1,58 @@
-import 'package:http/http.dart' as http;
+// 'jsonplaceholder.typicode.com', '/posts/2'
+
 import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 void main() async {
-  final post = await fetchPost();
-  print('Title: ${post.title}');
-  print('UserId: ${post.userId}');
+  var data = await fetchPost();
+  print(data.userId);
+  print(data.title);
 }
 
 Future<Post> fetchPost() async {
-  var uri = Uri.https('jsonplaceholder.typicode.com', '/posts/1');
+  var uri = Uri.http('jsonplaceholder.typicode.com', '/posts/3');
+  final response = await http.get(uri);
+  Map<String,dynamic> data = convert.jsonDecode(response.body);
 
-  final response = await http.get(uri, headers: {'Accept': 'application/json'});
+  return Post(userId: data["userId"], title: data["title"]);
 
-  if (response.statusCode != 200) {
-    throw Exception('Failed to fetch post: ${response.statusCode}');
-  }
-
-  Map<String, dynamic> data = convert.jsonDecode(response.body);
-
-  return Post(data["title"], data["userId"]);
 }
-
 class Post {
-  String title;
   int userId;
+  String title;
 
-  Post(this.title, this.userId);
+  Post({required this.userId, required this.title});
 }
+
+
+
+
+// import 'package:http/http.dart' as http;
+// import 'dart:convert' as convert;
+
+// void main() async {
+//   final post = await fetchPost();
+//   print('Title: ${post.title}');
+//   print('UserId: ${post.userId}');
+// }
+
+// Future<Post> fetchPost() async {
+//   var uri = Uri.https('jsonplaceholder.typicode.com', '/posts/1');
+
+//   final response = await http.get(uri, headers: {'Accept': 'application/json'});
+
+//   if (response.statusCode != 200) {
+//     throw Exception('Failed to fetch post: ${response.statusCode}');
+//   }
+
+//   Map<String, dynamic> data = convert.jsonDecode(response.body);
+
+//   return Post(data["title"], data["userId"]);
+// }
+
+// class Post {
+//   String title;
+//   int userId;
+
+//   Post(this.title, this.userId);
+// }
